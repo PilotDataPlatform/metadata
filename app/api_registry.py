58a3bfa1550525f67800e81dd28837d65bd9b050
Erik Taylor <etaylor@indocresearch.org>
2022-03-08 14:37:11 -0500

    Add health check

--- app/api_registry.py
+++ app/api_registry.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi_health import health
+from fastapi_sqlalchemy import db
 
 from .routers.v1 import api_hello_world
 
 
+def is_db_online():
+    return db.session is not None
+
+
 def api_registry(app: FastAPI):
     app.include_router(api_hello_world.router, prefix='/v1/hello-world', tags=['test'])
+    app.add_api_route('/v1/health', health([is_db_online]), tags=['health'])
