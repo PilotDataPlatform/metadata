f5cdf5d7970b50c2112cd8e05f3a91d56a0fad0a
Erik Taylor <etaylor@indocresearch.org>
2022-03-10 14:39:30 -0500

    Add all CRUD endpoints

--- app/routers/v1/attribute_templates/api_attribute_templates.py
+++ app/routers/v1/attribute_templates/api_attribute_templates.py
@@ -6,6 +6,18 @@ router = APIRouter()
 
 @cbv(router)
 class APIAttributeTemplates:
-    @router.get('/', summary='Get attribute template')
+    @router.get('/', summary='Get an attribute template')
     async def get_attribute_template(self):
+        return {'message': 'Hello world'}
+
+    @router.post('/', summary='Create a new attribute template')
+    async def create_attribute_template(self):
+        return {'message': 'Hello world'}
+
+    @router.put('/', summary='Update an attribute template')
+    async def update_attribute_template(self):
+        return {'message': 'Hello world'}
+
+    @router.delete('/', summary='Delete an attribute template')
+    async def delete_attribute_template(self):
         return {'message': 'Hello world'}
--- app/routers/v1/items/api_items.py
+++ app/routers/v1/items/api_items.py
@@ -6,6 +6,22 @@ router = APIRouter()
 
 @cbv(router)
 class APIItems:
-    @router.get('/', summary='Get item(s)')
+    @router.get('/', summary='Get one or more items or check if an item exists')
     async def get_items(self):
+        return {'message': 'Hello world'}
+
+    @router.post('/', summary='Create a new item')
+    async def create_item(self):
+        return {'message': 'Hello world'}
+
+    @router.put('/', summary='Update an item')
+    async def update_item(self):
+        return {'message': 'Hello world'}
+
+    @router.patch('/', summary='Move an item to the trash')
+    async def trash_item(self):
+        return {'message': 'Hello world'}
+
+    @router.delete('/', summary='Permanently delete an item')
+    async def delete_item(self):
         return {'message': 'Hello world'}
