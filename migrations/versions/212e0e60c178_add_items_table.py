e0578bf82af58230a3daf442f69efe20bcd2285a
Erik Taylor <etaylor@indocresearch.org>
2022-03-25 10:27:56 -0400

    Resolve DB enum conflicts

--- migrations/versions/212e0e60c178_add_items_table.py
+++ migrations/versions/212e0e60c178_add_items_table.py
@@ -6,7 +6,7 @@ Create Date: 2022-03-14 15:17:34.248801
 """
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy import Enum
+from sqlalchemy.dialects import postgresql as pg
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy_utils import LtreeType
 
@@ -25,14 +25,14 @@ def upgrade():
         sa.Column('path', LtreeType(), nullable=False),
         sa.Column('restore_path', LtreeType()),
         sa.Column('archived', sa.Boolean(), nullable=False),
-        sa.Column('type', Enum('file', 'folder', name='type_enum', create_type=False), nullable=False),
+        sa.Column('type', pg.ENUM('file', 'folder', name='type_enum', create_type=True), nullable=False),
         sa.Column('zone', sa.Integer(), nullable=False),
         sa.Column('name', sa.String(), nullable=False),
         sa.Column('size', sa.Integer()),
         sa.Column('owner', sa.String()),
         sa.Column('container', UUID()),
         sa.Column(
-            'container_type', Enum('project', 'dataset', name='container_enum', create_type=False), nullable=False
+            'container_type', pg.ENUM('project', 'dataset', name='container_enum', create_type=True), nullable=False
         ),
         sa.PrimaryKeyConstraint('id'),
         sa.UniqueConstraint('id'),
