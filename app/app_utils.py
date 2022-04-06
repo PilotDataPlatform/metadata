e8cf1170ff187515df6f84dc7dfb5f6eec390802
Erik Taylor <etaylor@indocresearch.org>
2022-04-06 16:47:01 -0400

    Switch to base32

--- app/app_utils.py
+++ app/app_utils.py
@@ -2,8 +2,8 @@ import base64
 import re
 
 def encode_label_for_ltree(raw_string: str) -> str:
-    base64_string = str(base64.b64encode(raw_string.encode('utf-8')), 'utf-8')
-    return re.sub('=', '', base64_string)
+    base32_string = str(base64.b32encode(raw_string.encode('utf-8')), 'utf-8')
+    return re.sub('=', '', base32_string)
 
 def encode_path_for_ltree(raw_path: str) -> str:
     labels = raw_path.split('.')
@@ -16,7 +16,7 @@ def decode_label_from_ltree(encoded_string: str) -> str:
     missing_padding = len(encoded_string) % 4
     if missing_padding:
         encoded_string += '=' * (4 - missing_padding)
-    utf8_string = base64.b64decode(encoded_string.encode('utf-8')).decode('utf-8')
+    utf8_string = base64.b32decode(encoded_string.encode('utf-8')).decode('utf-8')
     return utf8_string
 
 def decode_path_from_ltree(encoded_path: str) -> str:
